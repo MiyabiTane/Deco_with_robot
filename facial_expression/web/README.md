936af4e023f7ceccf034835e83c8064d06497e08
MiyabiTane <miyabitane@yahoo.co.jp>
2022-02-13 16:39:14 +0900

    複数webサイトで画像表示

---
+++ facial_expression/web/README.md
@@ -0,0 +1,20 @@
+##　FlaskでWebページ作成
+
+### 初回のみ
+```
+$ pipenv --python 3
+$ pipenv install flask
+$ pipenv install opencv-python
+$ pipenv install numpy
+```
+
+### 実行方法
+```
+$ pipenv run python app_1.py
+$ pipenv run python app_2.py
+$ pipenv run python app_3.py
+```
+以下の3つのページにアクセス<br>
+http://localhost:3000/<br>
+http://localhost:5000/<br>
+http://localhost:8000/<br>
---
+++ facial_expression/web/app_1.py
@@ -0,0 +1,19 @@
+import os
+import io
+import time
+import numpy as np
+import cv2
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
+app = Flask(__name__)
+
+UPLOAD_FOLDER = '/static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    raw_img_url = os.path.join(app.config['UPLOAD_FOLDER'], 'face.jpg')
+    return render_template('index.html', raw_img_url=raw_img_url)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='localhost')  # port: 5000
