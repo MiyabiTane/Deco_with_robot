83b8c684e2919b35aa9d1e0b3398e636d83de585
MiyabiTane <miyabitane@yahoo.co.jp>
2022-03-28 17:49:11 +0900

    ロボットの音声から表情変化

--- facial_expression/web_nodejs/sentiment_analysis_node.py
+++ facial_expression/web_nodejs/sentiment_analysis_node.py
@@ -4,6 +4,7 @@
 import rospy
 import actionlib
 import time
+import subprocess
 
 from ros_google_cloud_language.msg import AnalyzeTextAction, AnalyzeTextGoal
 from sound_play.msg import SoundRequest
@@ -16,17 +17,13 @@ class SentimentAnalysis:
         self.pub = rospy.Publisher("/degree", Float64, queue_size=1)
 
         rospy.Subscriber("/robotsound_jp", SoundRequest, self.sound_cb)
-        rospy.Subscriber("/flag", Bool, self.flag_cb)
         self.actionlib_client = actionlib.SimpleActionClient("/analyze_text/text", AnalyzeTextAction)
-
-    def flag_cb(self, msg):
-        self.flag = True
-
-    def sound_cb(self, msg):
-        self.flag = False
-        goal_msg = AnalyzeTextGoal(text=msg.arg)
         print("Waiting sever ...")
         self.actionlib_client.wait_for_server()
+
+
+    def sound_cb(self, msg):
+        goal_msg = AnalyzeTextGoal(text=msg.arg)
         self.actionlib_client.send_goal(goal_msg)
 
         print("Waiting goal ...")
@@ -38,10 +35,13 @@ class SentimentAnalysis:
         print("score: ", self.score)
         print("magnitude", analysis_result.sentiment_magnitude)
 
+        # [ToDo]self.pub.publishだと2回目以降が機能しない。原因解明
+        # pub_msg = Float64()
+        # pub_msg.data = self.score * 20
+        # self.pub.publish(pub_msg)
+
         # publish
-        pub_msg = Float64()
-        pub_msg.data = self.score * 20
-        self.pub.publish(pub_msg)
+        subprocess.call(["rostopic", "pub", "-1", "/degree", "std_msgs/Float64", "data: " + str(self.score * 20)])
 
 
 if __name__ == '__main__':
