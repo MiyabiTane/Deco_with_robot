4ff5055eee323bcc9b08b1bca3864ce844eb0555
MiyabiTane <miyabitane@yahoo.co.jp>
2022-04-16 15:05:12 +0900

    mode追加

--- facial_expression/web_nodejs/app/route/info.js
+++ facial_expression/web_nodejs/app/route/info.js
@@ -5,13 +5,16 @@ var degree = -20;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write(mode + ",");
   res.write(degree);
   res.end();
 });
 
 router.post('/', function(req, res, next) {
   degree = req.body.degree;
-  console.log("POSTED: ", degree);
+  mode = req.body.mode;
+  console.log("POSTED MODE: ", mode);
+  console.log("POSTED DEGREE:", degree);
 });
 
 module.exports = router;
