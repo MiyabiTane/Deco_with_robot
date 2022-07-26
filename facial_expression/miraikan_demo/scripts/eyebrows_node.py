3032cc0f6dd00f95a81260dd6ab8c113432503f8
MiyabiTane <miyabitane@yahoo.co.jp>
2022-07-26 19:56:58 +0900

    スマホで動かす際のバグ修正

--- facial_expression/miraikan_demo/scripts/eyebrows_node.py
+++ facial_expression/miraikan_demo/scripts/eyebrows_node.py
@@ -30,11 +30,7 @@ class TopicToEyebrows(object):
         """
         degrees = [0, 20, 90, 120, 50, 30, 20, 60, 70, 120, 70, 130, 50]
         wait_times = [5] * 13
-        wait_times[4] = 3.0
         wait_times[5] = 4.0
-        wait_times[7] = 3.0
-        wait_times[9] = 1.0
-        wait_times[10] = 1.0
         headers = {
             'Accept': 'application/json',
         }
