758d8c62114c34e9ca8e2c93a6ed3f25e2cecbb3
a-ichikura <ichikura@jsk.imi.i.u-tokyo.ac.jp>
2022-07-27 15:16:09 +0900

    add introduction, episode_0, summary part

--- facial_expression/miraikan_demo/scripts/episode_motion.py
+++ facial_expression/miraikan_demo/scripts/episode_motion.py
@@ -44,7 +44,7 @@ class Talk(object):
         
         #set init speech setting
         self.tts.setLanguage("Japanese")
-        self.tts.setParameter("pitchShift", 1.1)
+        self.tts.setParameter("pitch", 1.3)
         self.tts.setParameter("speed",90)
 
         #set animated say setting
@@ -59,7 +59,39 @@ class Talk(object):
         #  # [-2.802596928649634e-45, -0.20000003278255463]
         # angles = talk.mo.getAngles("Body", False)
         init_body_angles = [0.0, -2.802596928649634e-45, 1.5596766471862793, 0.14272688329219818, -1.228257656097412, -0.5225345492362976, -0.000497947505209595, 0.6000000238418579, 3.648194280003736e-08, -0.040683578699827194, -0.010746408253908157, 1.5596766471862793, -0.14272694289684296, 1.228257656097412, 0.5225345492362976, 0.0004979457589797676, 0.6000000238418579, 0.0, 0.0, 0.0]
-        self.mo.setAngles("Body", init_body_angles, 1.0)
+        self.mo.setAngles("Body", init_body_angles, 0.1)
+
+    def introduction(self):
+        #introduction func
+        time.sleep(1)
+        self.ans.say("^start(animations/Stand/Gestures/Me_1)私は、人型ロボットのペッパーです。",self.configuration)
+
+        time.sleep(2)
+        self.ans.say("^start(animations/Stand/Gestures/Explain_6)名前は、シナモンって言うよ。",self.configuration)
+        
+        time.sleep(2)
+        self.ans.say("コチさんと一緒に研究してきた、^start(animations/Stand/Gestures/Explain_11)コチさんの相棒だよ",self.configuration)
+
+        time.sleep(2)
+        self.ans.say("みんな、今日はどうぞよろしくね。^start(animations/Stand/Gestures/Hey_3)^wait(animations/Stand/Gestures/Hey_3)",self.configuration)
+
+        time.sleep(1)
+        self.set_init_posture()
+
+
+    def episode_01(self):
+        #episode 0-1                                                                                
+        time.sleep(1)
+        self.ans.say("コチさん、コチさんの困りゴトが、^start(animations/Stand/Gestures/You_3)この研究を始めたきっかけなんだよね",self.configuration)
+
+    def episode_02(self):
+        #episode 0-2                                                                                 
+        time.sleep(1)
+        self.mo.setStiffnesses(self.joint_names, 0)
+        self.ans.say("そうだね。^start(animations/Stand/Gestures/Me_1)私はコチさんが今でも、知らない人と話すときに少し苦労しているのを知っているよ。",self.configuration)
+
+        time.sleep(1)
+        self.set_init_posture()
 
     def episode_11(self):
 
@@ -104,6 +136,18 @@ class Talk(object):
         time.sleep(1)
         self.set_init_posture()
 
+    def episode_14(self):
+
+        #episode 1-4
+        time.sleep(1)
+        self.ans.say("3つのポイントって言っているけど、",self.configuration)
+
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("この３つにたどり着くまで^start(animations/Stand/Gestures/No_8)、色々大変だったんだよね",self.configuration)
+        time.sleep(1)
+        self.set_init_posture()
+
     def episode_21(self):
 
         #episode 2-1
@@ -123,7 +167,7 @@ class Talk(object):
         #episode 2-2
         self.mo.setStiffnesses(self.joint_names, 1)
         time.sleep(1)
-        self.ans.say("コチさんが^start(animations/Stand/Gestures/You_4)みんなに近づいてお話できるようにしてくれタから^wait(animations/Stand/Gesture/You_4)",self.configuration)
+        self.ans.say("コチさんが^start(animations/Stand/Gestures/You_4)みんなにツイていけるようにしてくれタから^wait(animations/Stand/Gesture/You_4)",self.configuration)
 
         self.mo.setStiffnesses(self.joint_names, 1)
         time.sleep(1)
@@ -302,14 +346,131 @@ class Talk(object):
         time.sleep(1)
         self.set_init_posture()
 
-"""
+    def summary_1(self):
+
+        #summary-1
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("わかったー",self.configuration)
+
+
+        time.sleep(1)
+        self.mo.setStiffnesses(self.joint_names, 1)
+        self.ans.say("今日は、ロボットによる人同士の交流づくりについて、コチさんとお話したよ",self.configuration)
+
+    def summary_2(self):
+
+        #summary-2
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("^start(animations/Stand/Gestures/Explain_3)まず、コチさんの夢、コチさんが研究を始めたきっかけ、ペッパーを使うようになった理由を話したね。",self.configuration)
+    
+    def summary_3(self):
+
+        #summary-3
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("次に、研究のポイントだね。",self.configuration)
+
+        time.sleep(1)
+        self.ans.say("交流づくりのポイントは、^start(animations/Stand/Gestures/You_1)接近すること",self.configuration)
+
+        time.sleep(0.3)
+        self.ans.say("^start(animations/Stand/Gestures/Give_4)人から世話を引き出すロボットにすること",self.configuration)
+
+        time.sleep(0.3)
+        self.ans.say("^start(animations/Stand/Gestures/Far_1)研究室を飛び出して科学館で研究したことだよ。",self.configuration)
+        
+        time.sleep(1)
+        self.set_init_posture()
+
+    def summary_4(self):
+
+        #summary-4
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("交流のこつは",self.configuration)
+        
+        time.sleep(0.3)
+        self.ans.say("^start(animations/Stand/Gestures/Give_4)共感を引き出すこと^wait(animations/Stand/Gestures/Give_4)",self.configuration)
+
+        time.sleep(0.3)
+        self.ans.say("^start(animations/Stand/Gestures/Far_2)移動",self.configuration)
+
+        time.sleep(0.3)
+        self.ans.say("^start(animations/Stand/Gestures/ShowTablet_2)触ってもらうことだとわかったよ",self.configuration)
+
+        time.sleep(1)
+        self.set_init_posture()
+
+    def summary_5(self):
+
+        #summary-5
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("発表の途中で",self.configuration)
+        
+        time.sleep(0.3)
+        self.ans.say("^start(animations/Stand/Gestures/ShowSky_8)客席に突進しちゃうこともあったね^wait(animations/Stand/Gestures/ShowSky_8)",self.configuration)
+
+        time.sleep(1)
+        self.set_init_posture()
+
+    def summary_6(self):
+
+        #summary-6
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("初めて誰かとお話しする場面で、",self.configuration)
+        
+        time.sleep(0.3)
+        self.ans.say("ロボットにどんなことをしてほしいか、^start(animations/Stand/Gestures/Please_1)みんなの意見も紹介したよ",self.configuration)
+        
+        time.sleep(1)
+        self.set_init_posture()
+
+
+    def summary_7(self):
+
+        #summary-7
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("^start(animations/Stand/Gestures/Everything_2)他にも色んなたいへんなことがあったけど",self.configuration)
+        
+        self.mo.setStiffnesses(self.joint_names, 0)
+        time.sleep(0.3)
+        self.ans.say("みんなのおかげで、^start(animations/Stand/Emotions/Positive/Peaceful_1)無事に博士論文をまとめられたんだね^wait(animations/Stand/Emotions/Positive/Peaceful_1)",self.configuration)        
+        time.sleep(1)
+        self.set_init_posture()
+
+    def summary_8(self):
+
+        #summary-8
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("この研究は、人の交流を手助けできるだけでなく、",self.configuration)
+        
+        time.sleep(0.3)
+        self.ans.say("人とロボットがナカよくなっていくためにも、",self.configuration)
+
+        time.sleep(0.3)
+        self.mo.setStiffnesses(self.joint_names, 0)
+        self.ans.say("^start(animations/Stand/Emotions/Positive/Peaceful_1)大切な研究だね。^wait(animations/Stand/Emotions/Positive/Peaceful_1)",self.configuration)
+
+        time.sleep(1)
+        self.mo.setStiffnesses(self.joint_names, 1)
+        self.set_init_posture()
+
+
+
 if __name__ == '__main__':
-    talk = Talk() #init
+    talk = Talk("169.254.175.13") #init
     val = input('input Number:')
     if val == 1:
         talk.episode_11()
         talk.episode_12()
         talk.episode_13()
+        talk.episode_14()
         exit()
     elif val == 2:
         talk.episode_21()
@@ -332,4 +493,20 @@ if __name__ == '__main__':
         talk.episode_53()
         talk.episode_54()
         exit()
-"""
+    elif val == 0:
+        talk.episode_01()
+        talk.episode_02()
+        exit()
+    elif val == 7:
+        talk.summary_1()
+        talk.summary_2()
+        talk.summary_3()
+        talk.summary_4()
+        talk.summary_5()
+        talk.summary_6()
+        talk.summary_7()
+        talk.summary_8()
+    elif val == 8:
+        talk.introduction()
+        exit()
+
--- facial_expression/miraikan_demo/scripts/explanation_motion.py
+++ facial_expression/miraikan_demo/scripts/explanation_motion.py
@@ -42,7 +42,7 @@ class Talk(object):
         
         #set init speech setting
         self.tts.setLanguage("Japanese")
-        self.tts.setParameter("pitchShift", 1.1)
+        self.tts.setParameter("pitch", 1.3)
         self.tts.setParameter("speed",90)
 
         #set animated say setting
@@ -57,7 +57,40 @@ class Talk(object):
         #  # [-2.802596928649634e-45, -0.20000003278255463]
         # angles = talk.mo.getAngles("Body", False)                                                 
         init_body_angles = [0.0, -2.802596928649634e-45, 1.5596766471862793, 0.14272688329219818, -1.228257656097412, -0.5225345492362976, -0.000497947505209595, 0.6000000238418579, 3.648194280003736e-08, -0.040683578699827194, -0.010746408253908157, 1.5596766471862793, -0.14272694289684296, 1.228257656097412, 0.5225345492362976, 0.0004979457589797676, 0.6000000238418579, 0.0, 0.0, 0.0]
-        self.mo.setAngles("Body", init_body_angles, 1.0)     
+        self.mo.setAngles("Body", init_body_angles, 0.1)     
+
+    def introduction(self):
+        #introduction func     
+        time.sleep(1)
+        self.ans.say("^start(animations/Stand/Gestures/Me_1)私は、人型ロボットのペッパーです。",self.configuration)
+
+        time.sleep(2)
+        self.ans.say("^start(animations/Stand/Gestures/Explain_6)名前は、ホップって言うよ。",self.configuration)
+
+        time.sleep(2)
+        self.ans.say("今日は、シナモンの代わりに^start(animations/Stand/Gestures/Explain_11)コチさんの発表をお手伝いするよ",self.configuration)
+
+        time.sleep(2)
+        self.ans.say("みんな、どうぞよろしくね。^start(animations/Stand/Gestures/Hey_3)^wait(animations/Stand/Gestures/Hey_3)",self.configuration)
+
+        time.sleep(1)
+        self.set_init_posture()
+
+        
+
+    def episode_01(self):
+        #episode 0-1
+        time.sleep(1)
+        self.ans.say("^コチさん、どうしてこの研究を始めたのー？",self.configuration)
+
+    def episode_02(self):
+        #episode 0-2
+        time.sleep(1)
+        self.ans.say("^へー、ソウなんだ。知らない人と話すのって緊張するもんね。",self.configuration)
+
+        time.sleep(1)
+        self.set_init_pose()
+        
         
     def episode_11(self):
 
@@ -70,9 +103,10 @@ class Talk(object):
 
         #episode 1-2
         time.sleep(1)
-        self.ans.say("へぇ！8年も前からなんだ",self.configuration)
+        self.ans.say("ソウなんだね^start(animations/Stand/Gestures/Yes_2)",self.configuration)
+  
 
-        time.sleep(1)
+        time.sleep(3)
         self.ans.say("研究室には、色んなロボットがいるみたいダケド、",self.configuration)
         time.sleep(1)
         self.ans.say("どうしてペッパーを使おうと思ったの？",self.configuration)
@@ -89,23 +123,29 @@ class Talk(object):
         time.sleep(1)
         self.set_init_posture()
 
+    def episode_14(self):
+
+        #episode 1-4
+        time.sleep(1)
+        self.ans.say("すごい！３つのポイント、わかりやすいね",self.configuration)
+
+        time.sleep(1)
+        self.set_init_posture()
+        
+        
     def episode_21(self):
 
         #episode 2-1
-        time.sleep(1)
-        self.ans.say("未来館ではどんなことがあったのー？",self.configuration)
-
-        time.sleep(1)
-        self.pos.goToPosture("StandInit", 1.0)
+        print("dummy func")
 
     def episode_22(self):
 
         #episode 2-2
         time.sleep(1)
-        self.ans.say("たくさんの子どもたちが集まっているね",self.configuration)
+        self.ans.say("ペッパーの手を、とってくれているコがいるね.",self.configuration)
 
         time.sleep(1)
-        self.ans.say("ロボットが近づいた時、みんな嬉しそうな顔をしているね！",self.configuration)
+        self.ans.say("みんな、嬉しそうな様子だね.",self.configuration)
 
         time.sleep(1)
         self.set_init_posture()
@@ -113,35 +153,22 @@ class Talk(object):
     def episode_23(self):
 
         #episode 2-3
-        time.sleep(1)
-        self.ans.say("わたしもみんなの笑顔を、見てみたいナァ",self.configuration)
-
-        time.sleep(1)
-        self.set_init_posture()
+        print("dummy func")
 
     def episode_31(self):
 
         #episode 3-1
         time.sleep(1)
-        self.ans.say("発表の場所までの移動の仕方を教えてー！",self.configuration)
+        self.ans.say("発表の場所までの移動の仕方を教えて！",self.configuration)
 
         time.sleep(1)
         self.set_init_posture()
 
-    def episode_32_1(self):
+    def episode_32(self):
 
-        #episode 3-2-1
+        #episode 3-2
         time.sleep(1)
         self.ans.say("コチさんが連れて行っていたんだねー！",self.configuration)
-
-        time.sleep(1)
-        self.set_init_posture()
-
-    def episode_32_2(self):
-
-        #episode 3-2-2
-        time.sleep(1)
-        self.ans.say("ロボットを操作するのは大変？",self.configuration)
 
         time.sleep(1)
         self.set_init_posture()
@@ -153,10 +180,7 @@ class Talk(object):
         self.ans.say("ねぇねぇ、コチさん",self.configuration)
 
         time.sleep(1)
-        self.ans.say("ロボットが背負っているリュックにも、意味があるって聞いたんだけど、",self.configuration)
-
-        time.sleep(1)
-        self.ans.say("どんな意味があるのー？",self.configuration)
+        self.ans.say("ペッパーは、どうしてリュックを背負っているのー？",self.configuration)
 
         time.sleep(1)
         self.set_init_posture()
@@ -210,7 +234,7 @@ class Talk(object):
 
         #episode 5-2
         time.sleep(1)
-        self.ans.say("どうして成果をまとめるのが大変だったの？",self.configuration)
+        self.ans.say("どうして大変だったの？",self.configuration)
  
         time.sleep(1)
         self.set_init_posture()
@@ -219,10 +243,7 @@ class Talk(object):
 
         #episode 5-3
         time.sleep(1)
-        self.ans.say("人とロボットの交流を作るのって、すごく大変ダケド、",self.configuration)
-
-        time.sleep(1)
-        self.ans.say("大切な研究であることがよくわかったよ",self.configuration)
+        self.ans.say("そっかー、それは大変だったね。",self.configuration)
 
         time.sleep(1)
         self.set_init_posture()
@@ -231,7 +252,7 @@ class Talk(object):
 
         #episode 5-4-1
         time.sleep(1)
-        self.ans.say("コチさんはペッパーと8年間もこの研究をしているんだね",self.configuration)
+        self.ans.say("でもコチさん、８年間たくさん頑張ったんだね。",self.configuration)
 
         time.sleep(1)
         self.set_init_posture()
@@ -239,49 +260,181 @@ class Talk(object):
     def episode_54_2(self):
 
         #episode 5-4-2
-        time.sleep(1)
-        self.ans.say("わたし達ロボットがもっと社会でお友達を作るために、必要なことを研究しているんだなぁ",self.configuration)
-
-        time.sleep(1)
-        self.set_init_posture()
+        print("dummy func")
 
     def episode_54_3(self):
 
         #episode 5-4-3
         time.sleep(1)
-        self.ans.say("コチさん、今日は説明してくれてありがとう。これからも研究がんばってね",self.configuration)
+        self.ans.say("コチさん、博士の卒業、本当におめでとう。")
+        
+        time.sleep(1)
+        self.ans.say("今日は説明してくれてありがとう。これからも研究がんばってね",self.configuration)
 
         time.sleep(1)
         self.set_init_posture()
 
-"""
+    def summary_1(self):
+
+        #summary-1                                                                                                                                                                                          
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("わかったー",self.configuration)
+
+
+        time.sleep(1)
+        self.mo.setStiffnesses(self.joint_names, 1)
+        self.ans.say("今日は、ロボットによる人同士の交流づくりについて、コチさんとお話したよ",self.configuration)
+
+    def summary_2(self):
+
+        #summary-2                                                                                                                                                                                          
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("^start(animations/Stand/Gestures/Explain_3)まず、コチさんの夢、コチさんが研究を始めたきっかけ、ペッパーを使うようになった理由を話したね。",self.configuration)
+
+    def summary_3(self):
+
+        #summary-3                                                                                                                                                                                          
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("次に、研究のポイントだね。",self.configuration)
+
+        time.sleep(1)
+        self.ans.say("交流づくりのポイントは、^start(animations/Stand/Gestures/You_1)接近すること",self.configuration)
+
+        time.sleep(0.3)
+        self.ans.say("^start(animations/Stand/Gestures/Give_4)人から世話を引き出すロボットにすること",self.configuration)
+
+        time.sleep(0.3)
+        self.ans.say("^start(animations/Stand/Gestures/Far_1)研究室を飛び出して科学館で研究したことだよ。",self.configuration)
+
+        time.sleep(1)
+        self.set_init_posture()
+
+
+    def summary_4(self):
+
+        #summary-4                                                                                                                                                                                          
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("交流のこつは",self.configuration)
+
+        time.sleep(0.3)
+	self.ans.say("^start(animations/Stand/Gestures/Give_4)共感を引き出すこと^wait(animations/Stand/Gestures/Give_4)",self.configuration)
+
+        time.sleep(0.3)
+	self.ans.say("^start(animations/Stand/Gestures/Far_2)移動",self.configuration)
+
+	time.sleep(0.3)
+        self.ans.say("^start(animations/Stand/Gestures/ShowTablet_2)触ってもらうことだとわかったよ",self.configuration)
+
+        time.sleep(1)
+        self.set_init_posture()
+    def summary_5(self):
+
+        #summary-5                                                                                                                                                                                          
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("発表の途中で",self.configuration)
+
+        time.sleep(0.3)
+        self.ans.say("^start(animations/Stand/Gestures/ShowSky_8)客席に突進しちゃうこともあったね^wait(animations/Stand/Gestures/ShowSky_8)",self.configuration)
+
+        time.sleep(1)
+        self.set_init_posture()
+
+    def summary_6(self):
+
+        #summary-6                                                                                                                                                                                          
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("初めて誰かとお話しする場面で、",self.configuration)
+
+        time.sleep(0.3)
+        self.ans.say("ロボットにどんなことをしてほしいか、^start(animations/Stand/Gestures/Please_1)みんなの意見も紹介したよ",self.configuration)
+
+        time.sleep(1)
+        self.set_init_posture()
+
+    def summary_7(self):
+
+        #summary-7                                                                                                                                                                                          
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("^start(animations/Stand/Gestures/Everything_2)他にも色んなたいへんなことがあったけど",self.configuration)
+
+        self.mo.setStiffnesses(self.joint_names, 0)
+        time.sleep(0.3)
+        self.ans.say("みんなのおかげで、^start(animations/Stand/Emotions/Positive/Peaceful_1)無事に博士論文をまとめられたんだね^wait(animations/Stand/Emotions/Positive/Peaceful_1)",self.configuration)   
+        time.sleep(1)
+        self.set_init_posture()
+
+    def summary_8(self):
+
+        #summary-8                                                                                                                                                                                          
+        self.mo.setStiffnesses(self.joint_names, 1)
+        time.sleep(1)
+        self.ans.say("この研究は、人の交流を手助けできるだけでなく、",self.configuration)
+
+        time.sleep(0.3)
+        self.ans.say("人とロボットがナカよくなっていくためにも、",self.configuration)
+
+        time.sleep(0.3)
+        self.mo.setStiffnesses(self.joint_names, 0)
+        self.ans.say("^start(animations/Stand/Emotions/Positive/Peaceful_1)大切な研究だね。^wait(animations/Stand/Emotions/Positive/Peaceful_1)",self.configuration)
+
+        time.sleep(1)
+        self.mo.setStiffnesses(self.joint_names, 1)
+        self.set_init_posture()
+
+
+
 if __name__ == '__main__':
-    talk = Talk() #init
+    talk = Talk("169.254.175.13") #init                                                                                                                                                                     
     val = input('input Number:')
     if val == 1:
         talk.episode_11()
         talk.episode_12()
         talk.episode_13()
+        talk.episode_14()
         exit()
     elif val == 2:
         talk.episode_21()
-        talk.episode_22()
+	talk.episode_22()
         talk.episode_23()
         exit()
     elif val == 3:
-        talk.episode_31()
+	talk.episode_31()
         talk.episode_32()
         talk.episode_33()
         exit()
     elif val == 4:
-        talk.episode_41()
-        talk.episode_42()
-        talk.episode_43()
-        exit()
+	talk.episode_41()
+	talk.episode_42()
+	talk.episode_43()
+	exit()
     elif val == 5:
-        talk.episode_51()
-        talk.episode_52()
-        talk.episode_53()
-        talk.episode_54()
+	talk.episode_51()
+	talk.episode_52()
+	talk.episode_53()
+	talk.episode_54()
+	exit()
+    elif val == 0:
+	talk.episode_01()
+	talk.episode_02()
+	exit()
+    elif val == 7:
+	talk.summary_1()
+	talk.summary_2()
+	talk.summary_3()
+	talk.summary_4()
+	talk.summary_5()
+	talk.summary_6()
+	talk.summary_7()
+	talk.summary_8()
         exit()
-"""
+    elif val== 8:
+        talk.introduction()
+        exit()
+        
